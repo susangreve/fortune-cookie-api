ce97916b2f91c59ba0cfab8291d827adaf570d49
David Middlecamp <david@spark.io>
2016-10-03 21:05:43 -0500

    boom, schaka laka.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
 
+var fs = require('fs');
+
+console.log("I'm a horrible human being.")
+var theFile = fs.readFileSync("../public/fortunes/fortunes.txt").toString();
+global.fortunes = theFile.split("%");
+console.log("all done");
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+/* GET fortune  */
+router.get('/api/fortune', function(req, res, next) {
+	var fortune;
+	var numLines = global.fortunes.length;
+	var index = Math.round(Math.random() * numLines);
+	var line = global.fortunes[index];
+
+	if (line) {
+		line = line.trim();
+	}
+
+	res.send({
+		ok: true,
+		message: line
+	});
+});
+
+
 
 module.exports = router;
