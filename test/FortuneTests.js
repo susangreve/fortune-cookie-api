41a580df7f58049240ba64e26df037ca7c7757db
David Middlecamp <david@spark.io>
2016-10-04 20:28:37 -0500

    adding mocha / unit tests~!

---
+++ test/FortuneTests.js
@@ -0,0 +1,17 @@
+var assert = require('assert');
+
+
+
+describe('fortuneApi', function() {
+
+    it('should be a unit test', function() {
+
+		var onePlusOne = 1 + 1;
+		var two = 2;
+
+
+		console.log("did it work?");
+		assert.equal(onePlusOne, two);
+
+    });
+});
